f299828cc6461650f4c27574c6eb3265edf2df6c
Jen Poole <contact.vandepool@gmail.com>
2018-02-03 00:39:07 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,9 +15,23 @@ def create_project_hash
 
    # iterate through the projects
    # each project title is a key, and its value is another hash with each of the other data points as keys
-   kickstarter.css("li.project grid_4").each do |project|
+   kickstarter.css("li.project.grid_4").each do |project|
      title = project.css("h2.bbcard_name strong a").text
-     projects[title.to_sym] = {} # project titles point to a hash of info. Note: Convert title into symbol using #to_sym. Symbols make better hash keys than strings
+
+     # project titles point to a hash of info. 
+     # Note: Convert title into symbol using #to_sym. Symbols make better hash keys than strings.
+
+     # add data points to each project's hash
+       # each project has an image link hosted on AmazonAWS
+       # each project has a description which is a string
+       # each project has a location which is a string
+       # each project has percentage funded listed which is an integer
+     projects[title.to_sym] = {
+       :image_link => project.css(".project-thumbnail a img").attribute("src").value,
+       :description => project.css("p.bbcard_blurb").text,
+       :location => project.css("ul.project-meta li a span.location-name").text,
+       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+     }  
    end
 
    # return the projects hash
@@ -26,17 +40,14 @@ end
 
 
 # selecting elements
-  # projects: kickstarter.css("li.project grid_4")
+  # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
-  # image: project.css(".project-thumbnail a img").attribute("src").value
+  # image_link: project.css(".project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("ul.project-meta li a span.location-name").text
-  # funding: projet.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 
 
 
-# each project has an image link hosted on AmazonAWS
-# each project has a description which is a string
-# each project has a location which is a string
-# each project has percentage funded listed which is an integer
+
